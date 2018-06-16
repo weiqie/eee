76651060bc57523617a8018e270886a5441d2bec
weiqie <865765894@qq.com>
2018-06-16 23:07:43 +0800

    图片切换

---
+++ 图片切换/tools.js
@@ -0,0 +1,217 @@
+/*
+	options:
+		el 要运动的元素    *
+		attr 要运动的属性  *
+		target 目标点      *
+
+		duration 持续时间 默认400
+		fx 运行形式 默认匀速 linear
+
+*/
+function css(el,attr,value){
+	if(arguments.length === 2){
+		return getComputedStyle(el)[attr]
+	}else {
+		el.style[attr]  = value + 'px';
+	}
+}
+function mTween(options){
+
+	// 如果元素上已经有定时在开启，不再继续开启了
+	if(options.el['timer_'+options.attr]){
+		return;
+	}
+
+	var defaults = {
+		el: null,
+		attr: null,
+		target: null,
+		duration: 400,
+		fx: 'linear',
+		callback: function(){}
+	}
+
+	for(var at in options){
+		defaults[at] = options[at]
+	}
+
+	// 存在变量中
+	var 
+		el = defaults.el,
+		attr = defaults.attr,
+		target = defaults.target,
+		duration = defaults.duration,
+		callback = defaults.callback,
+		fx = defaults.fx;
+
+
+	// 运动函数
+	// 已过去时间所处的位置 = 总距离/持续时间 * 已过去时间 + 起始位置
+	var begin = parseFloat(css(el,attr))
+	var count = target - begin;
+
+	var start = Date.now(); // 开始
+
+	el['timer_'+attr] = setInterval(function (){
+		var time = Date.now() - start;	// 已过去时间
+		// 401 400
+		if(time > duration){
+			time = duration;
+			clearInterval(el['timer_'+attr])
+			el['timer_'+attr] = null;
+			
+		}
+		var value = Tween[fx](time,begin,count,duration);
+
+		el.style[attr]  = value + 'px'
+
+		// 等到终点赋值给元素之后，再执行callback
+		if(time === duration){
+			callback();
+		}
+
+	})
+}
+
+/*
+* t : time 已过时间
+* b : begin 起始值
+* c : count 总的运动值
+* d : duration 持续时间
+*
+* 曲线方程
+*
+* http://www.cnblogs.com/bluedream2009/archive/2010/06/19/1760909.html
+* */
+
+//Tween.linear();
+
+var Tween = {
+	linear: function (t, b, c, d){  //匀速
+		return c*t/d + b;
+	},
+	easeIn: function(t, b, c, d){  //加速曲线
+		return c*(t/=d)*t + b;
+	},
+	easeOut: function(t, b, c, d){  //减速曲线
+		return -c *(t/=d)*(t-2) + b;
+	},
+	easeBoth: function(t, b, c, d){  //加速减速曲线
+		if ((t/=d/2) < 1) {
+			return c/2*t*t + b;
+		}
+		return -c/2 * ((--t)*(t-2) - 1) + b;
+	},
+	easeInStrong: function(t, b, c, d){  //加加速曲线
+		return c*(t/=d)*t*t*t + b;
+	},
+	easeOutStrong: function(t, b, c, d){  //减减速曲线
+		return -c * ((t=t/d-1)*t*t*t - 1) + b;
+	},
+	easeBothStrong: function(t, b, c, d){  //加加速减减速曲线
+		if ((t/=d/2) < 1) {
+			return c/2*t*t*t*t + b;
+		}
+		return -c/2 * ((t-=2)*t*t*t - 2) + b;
+	},
+	elasticIn: function(t, b, c, d, a, p){  //正弦衰减曲线（弹动渐入）
+		if (t === 0) { 
+			return b; 
+		}
+		if ( (t /= d) == 1 ) {
+			return b+c; 
+		}
+		if (!p) {
+			p=d*0.3; 
+		}
+		if (!a || a < Math.abs(c)) {
+			a = c; 
+			var s = p/4;
+		} else {
+			var s = p/(2*Math.PI) * Math.asin (c/a);
+		}
+		return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+	},
+	elasticOut: function(t, b, c, d, a, p){    //*正弦增强曲线（弹动渐出）
+		if (t === 0) {
+			return b;
+		}
+		if ( (t /= d) == 1 ) {
+			return b+c;
+		}
+		if (!p) {
+			p=d*0.3;
+		}
+		if (!a || a < Math.abs(c)) {
+			a = c;
+			var s = p / 4;
+		} else {
+			var s = p/(2*Math.PI) * Math.asin (c/a);
+		}
+		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+	},    
+	elasticBoth: function(t, b, c, d, a, p){
+		if (t === 0) {
+			return b;
+		}
+		if ( (t /= d/2) == 2 ) {
+			return b+c;
+		}
+		if (!p) {
+			p = d*(0.3*1.5);
+		}
+		if ( !a || a < Math.abs(c) ) {
+			a = c; 
+			var s = p/4;
+		}
+		else {
+			var s = p/(2*Math.PI) * Math.asin (c/a);
+		}
+		if (t < 1) {
+			return - 0.5*(a*Math.pow(2,10*(t-=1)) * 
+					Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+		}
+		return a*Math.pow(2,-10*(t-=1)) * 
+				Math.sin( (t*d-s)*(2*Math.PI)/p )*0.5 + c + b;
+	},
+	backIn: function(t, b, c, d, s){     //回退加速（回退渐入）
+		if (typeof s == 'undefined') {
+		   s = 1.70158;
+		}
+		return c*(t/=d)*t*((s+1)*t - s) + b;
+	},
+	backOut: function(t, b, c, d, s){
+		if (typeof s == 'undefined') {
+			s = 3.70158;  //回缩的距离
+		}
+		return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+	}, 
+	backBoth: function(t, b, c, d, s){
+		if (typeof s == 'undefined') {
+			s = 1.70158; 
+		}
+		if ((t /= d/2 ) < 1) {
+			return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+		}
+		return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+	},
+	bounceIn: function(t, b, c, d){    //弹球减振（弹球渐出）
+		return c - Tween['bounceOut'](d-t, 0, c, d) + b;
+	},       
+	bounceOut: function(t, b, c, d){//*
+		if ((t/=d) < (1/2.75)) {
+			return c*(7.5625*t*t) + b;
+		} else if (t < (2/2.75)) {
+			return c*(7.5625*(t-=(1.5/2.75))*t + 0.75) + b;
+		} else if (t < (2.5/2.75)) {
+			return c*(7.5625*(t-=(2.25/2.75))*t + 0.9375) + b;
+		}
+		return c*(7.5625*(t-=(2.625/2.75))*t + 0.984375) + b;
+	},      
+	bounceBoth: function(t, b, c, d){
+		if (t < d/2) {
+			return Tween['bounceIn'](t*2, 0, c, d) * 0.5 + b;
+		}
+		return Tween['bounceOut'](t*2-d, 0, c, d) * 0.5 + c*0.5 + b;
+	}
+}
