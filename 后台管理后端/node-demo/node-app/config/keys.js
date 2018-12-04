dcdd813e7cbb9815ca57ca78aa11cfe6c8ccfcee
weiqie <865765894@qq.com>
2018-12-05 00:35:26 +0800

    后台管理系统

---
+++ 后台管理后端/node-demo/node-app/config/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+  // mongoURI:"mongodb://mr-qie:qw113828@ds125821.mlab.com:25821/qie-api",
+  mongoURI:"mongodb://qie-vue:qw123456@ds123896.mlab.com:23896/qie-vue",
+  secretOrKey:"secret"
+}
+const mongoose = require('mongoose')
